f60532fb52665c7c024f8c97d3b8251fbf501a2e
NoemiCorsi96 <noemicorsi96@gmail.com>
2025-07-18 18:05:36 +0200

    es4

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -5,8 +5,24 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+function numeroVocali(stringa) {
+    //let stringa = '';
+    let vocali = ['a', 'e', 'i', 'o', 'u'];
+    let contatore = 0;
+    for (let i = 0; i < stringa.length; i++){
+         for (let index = 0; index < vocali.length; index ++){
+            
+            if (stringa[i] == vocali[index]) {
+                contatore = contatore + 1;
+            } 
+        }
+    }
+    return contatore
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+const contaVocali= console.log(numeroVocali(word));
+
 
 
 
