2a237e67f02fae4fd4d1abf4bb16056e01693478
NoemiCorsi96 <noemicorsi96@gmail.com>
2025-07-18 11:27:35 +0200

    es2

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -5,9 +5,19 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
-
+function restituisco_iniziali(array) {
+    const iniziali = [];
+    for (let i = 0; i < array.length; i++) {
+        iniziali.push(array[i][0]); 
+    }
+    return iniziali;
+}
+//il doppio indice mi fa ciclare sia sulla posizione delle stringhe che sulla posizione delle lettere nella stringa
+//In questo caso ciclo sulle stringhe, mentre tengo fermo [0] per indicare la prima lettera di ogni stringa
 // Invoca la funzione qui e stampa il risultato in console
 
+const array_iniziali = restituisco_iniziali(names);
+console.log(array_iniziali);
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
